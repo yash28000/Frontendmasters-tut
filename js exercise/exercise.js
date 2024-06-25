139df9986dbc16b5ebc234d82e73e83796c1c19b
Yash Singh <yashsingh.2803@gmail.com>
2024-06-25 23:41:06 +0530

    some change

--- js exercise/exercise.js
+++ js exercise/exercise.js
@@ -3,7 +3,16 @@
 function printCharacter(character, timesToRepeat){
     for (let i=0; i<timesToRepeat; i++){
         console.log(character);
+        const pizza ="11"
+        console.log(pizza);
     }
 }
 
-printCharacter("a", 5);
+printCharacter("a", 6);
+console.log(pizza);
+
+const menu =function(){
+    console.log("food to eat");
+}
+
+console.log(menu());
