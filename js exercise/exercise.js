1064291de52c5883aa71f19551bb9bc83d7220d0
Yash Singh <yashsingh.2803@gmail.com>
2024-06-26 15:24:07 +0530

    some change

--- js exercise/exercise.js
+++ js exercise/exercise.js
@@ -1,18 +1,22 @@
-
-
-function printCharacter(character, timesToRepeat){
-    for (let i=0; i<timesToRepeat; i++){
-        console.log(character);
-        const pizza ="11"
-        console.log(pizza);
-    }
-}
-
-printCharacter("a", 6);
-console.log(pizza);
-
-const menu =function(){
-    console.log("food to eat");
-}
-
-console.log(menu());
+const me = {
+    name: {
+      first: "Brian",
+      last: "Holt",
+    },
+    location: {
+      streetNumber: 500,
+      street: "Fakestreet",
+      city: "Seattle",
+      state: "WA",
+      zipCode: 55555,
+      country: "USA",
+    },
+    getAddress() {
+      return `${this.name.first} ${this.name.last}
+  ${this.location.streetNumber} ${this.location.street}
+  ${this.location.city}, ${this.location.state} ${this.location.zipCode}
+  ${this.location.country}`;
+    },
+  };
+  
+  console.log(me.getAddress());
